7123f1e378c11ae3ac1c32474deeab0327ea3ac7
saarakhalid <khalidsaara004@gmail.com>
2024-01-04 17:12:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+    });
+
+    input.addEventListener('click', clickAlert);
 }
+
